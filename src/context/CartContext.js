c8a5747c4ede27f7febf32b845acd64af8ee941b
Natee Rudkui <nateerudkui@outlook.com>
2023-09-22 11:41:38 +0700

    ✅ : add formatMoney

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -11,8 +11,13 @@ const initState = {
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initState);
+  function formatMoney(money) {
+    return money.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  }
   return (
-    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, formatMoney }}>
+      {children}
+    </CartContext.Provider>
   );
 };
 
