b2d0f1216e1d23f10b70de380794b401f827a9ff
Natee Rudkui <nateerudkui@outlook.com>
2023-09-22 10:42:49 +0700

    ✅ : use CartProvider all app

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,6 +1,20 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import products from "../data/products";
+import cartReducer from "../reducer/cartReducer";
 
 const CartContext = createContext();
+const initState = {
+  products: products,
+  totol: 0,
+  amount: 0,
+};
+
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initState);
+  return (
+    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+  );
+};
 
 export const useCart = () => {
   return useContext(CartContext);
